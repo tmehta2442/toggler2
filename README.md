24989b2d1ff83eda51de03a39e86c4587fd047b0
tmehta2442 <tmehta@post.harvard.edu>
2013-11-29 18:02:44 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Another of my "for-fun" apps:
+
+Create a simple rails blog app. 
+
+It will only be able to take in a user name and text field... a fun project for after Thanksgiving!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toggler2::Application.config.secret_token = '838dfc901c3c5d55c6edad8aeb341e2fe6e755f37962ec27d908533dc425dc6f2f860750690fceb7e24790a1dab5a67aafd9f5705ca7b7e580c00c49aab8edfa'
